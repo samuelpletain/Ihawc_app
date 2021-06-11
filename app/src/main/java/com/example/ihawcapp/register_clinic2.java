0ff9e6f35115defd6864f34e869bd435eae392cc
robert-bridge <robert.bridge@salesforce.com>
2021-06-11 00:14:38 -0400

    added the blank layout activities

---
+++ app/src/main/java/com/example/ihawcapp/register_clinic2.java
@@ -0,0 +1,14 @@
+package com.example.ihawcapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class register_clinic2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_clinic2);
+    }
+}
