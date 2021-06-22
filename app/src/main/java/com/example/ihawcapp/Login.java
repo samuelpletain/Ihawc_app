f5012ce88b00a6636b39bfcd2ef98cfebd894262
robert-bridge <robert.bridge@salesforce.com>
2021-06-22 19:35:13 -0400

    Changed Default view

--- app/src/main/java/com/example/ihawcapp/Login.java
+++ app/src/main/java/com/example/ihawcapp/Login.java
@@ -2,13 +2,30 @@ package com.example.ihawcapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity implements View.OnClickListener {
+    private TextView register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login1);
+
+        register = (TextView)findViewById(R.id.register);
+        register.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.register:
+                startActivity(new Intent(this, Register.class));
+                break;
+        }
     }
 }
